b32899b10c33de7223fd2a7a28d065e0be2681a0
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2024-12-15 02:56:41 +0300

    update

--- src_oauth2_slim/src/auth/auth20.rb
+++ src_oauth2_slim/src/auth/auth20.rb
@@ -96,7 +96,7 @@ module Auth20
         # OR Content-Type: application/jwt
         {
           id: 'admin',
-          role: 'admin',
+          role: 'Admin',
           sub: "admin",
           name: "Alice Adams",
           email: "alice@example.com",
