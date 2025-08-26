539b541147a757f039c880bb655928de68b695d8
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2025-08-26 22:07:03 +0300

    update

--- src/auth/auth_forward.rb
+++ src/auth/auth_forward.rb
@@ -7,6 +7,14 @@ require 'faraday'
 
 $stdout.sync=true
 FORWARD_AUTH = {}
+KEY_TEXT = "-----BEGIN RSA PRIVATE KEY-----\n MIIEowIBAAKCAQEA78te1DlUytbaOmSJt2v7jammv1+DYlS3Q+KlJjlKrABxvpYT WTeS/NfuY2tq3OBe/9BuEB2WS5laEJWpQV4ypEhN5agFgTPRNTmErY75adqWsf3t ISKzLTeIdjCgM1BXlkcykJA44Wi++KOsy90ZC6K+tkzRiCFac6eMuXK+zmc6U+6k Sz1uivJ17HABybzi9dVodsObRPJX4ymP85NMgTGNPM0KSKWrC5S1LvOFIHBfID4D C0tqj/5po/g8c50wpAT9yL/+qjkDLs1u0rnCKJAjwhWVSa2Wh2ZFKDk62TDm73JI QwxGzqqxWUGfKCg8JgcS6EQYu952hHYcsQdXoQIDAQABAoIBADjnhcwug77z8Ash 3yWKKEK0owewkpTj3i6rqv3QZDY/j76GzjYikEzUsDNjIxOh4iFqVKgZ4Vf03xDm 9yi2QiXGq8enUREZWxj6mo/2SR1T3GWGSP7PRX5iOo2zmdy7gOF+aVMxinIBYapO 6xtgz0f52bCYt7OKmLSv6t76SingEzk9FGTlCtCaKMyoHEA8qC+WKNqTEndh2DOn oMGX0zPWTmN/9P+epBGAxRVpYkmtCVQlY4u85zteuxs8SWSnkhbwwCazzapqOqKe f/lr/KJiFZ42ta6Zzh4zUvqvA2S8IrP33wm86x7ZyaBhJ8buVKEkZ4Fm5RdEPun4 Uo+Rm+0CgYEA9/dmzwdWfMmGRJoc3rdDJelbVhQeJ8eGNlOnvs4KEUK+HITEzf+u 7F3PohRF8HmgguQKuX6exLkCME9/mAomAqivitrB5aH93ubMjYzleFbwvZRLQgA7 zitdAwdoLKdw2RTo1JQtnw+I+dFDiUyolPANtGlQRJPsk56C+gBekAUCgYEA95Ax Al0byJ9Fdl91LM2MUdc3hLAzqpFnlA6DELnwkx7axshMPh5+O7+cmLMskCDIuXrI w2KNbkKSlpJituxFR37eiSgKsT13amf1pOA35iTA5pAWf6jgD6zWmhK599c/xwFe 1txa9tgCZZVSLRT7RpQ/bFWScY7puRWsaW+CZ+0CgYBkM4pM8KcnZ/wk7q3p5d5x jHoHL7v70SnP/EAV34a78N+IALLSl5alF0eXNGAKy+tr2SDoUl1wG17iDM3/r2Iq wuWk0790vuAq2dMhBCWaWm+P/EPpGNUR+/3rAmw7VzJH1qY7eOFynEF6yfBjpCGf hd3T4Ja6D5iTEoPU6J9NzQKBgQD2SF5ZSaOQTNLf1ktNzRchfAfhWyGrOIhgxKcj BrgMxI9FXpJq4g8XBaKWTvmwUM0fqMT9i5fD7zrBzNOjwx2Q6OgebtVkSg/4rX+1 DNLPhBTbytB3I9vz/DBqiuKza1IpenWNLx7Xn0GTKZ/c9ZidOHJ4JhFQI6rk1Gj3 Y1XKaQKBgB2GSNS5kkAQra12wZbmKSH61yzIxWtv58txy31iTguhSsQy6HaFvBFZ sRU0N3JOGqm8djALzJy/4nNmBoeOUWLy7a16w5qH5ED5CWGL7mtWsZ5+Sn+G9ECB XaWMJ1SovPLMiNg7dxlWuWQbXRI26bN5BBZVkAdUl0ps1KBDeogM\n-----END RSA PRIVATE KEY-----"
+PRIVATE_KEY = OpenSSL::PKey::RSA.new(KEY_TEXT)
+PUBLIC_KEY = PRIVATE_KEY.public_key
+FORWARD_OAUTH_AUTH_URL = ENV['FORWARD_OAUTH_AUTH_URL']
+AUTH_VERIFY_KEY = ENV['AUTH_VERIFY_KEY']
+AUTH_SCOPE = ENV['AUTH_SCOPE']
+AUTH_BOT = ENV['AUTH_BOT']
+AUTH_CODES = {}
 
 def forward_auth(&block)
   FORWARD_AUTH[:method] = lambda do
@@ -18,26 +26,24 @@ def forward_auth(&block)
 end
 
 module AuthForward
-  KEY_TEXT = "-----BEGIN RSA PRIVATE KEY-----\n MIIEowIBAAKCAQEA78te1DlUytbaOmSJt2v7jammv1+DYlS3Q+KlJjlKrABxvpYT WTeS/NfuY2tq3OBe/9BuEB2WS5laEJWpQV4ypEhN5agFgTPRNTmErY75adqWsf3t ISKzLTeIdjCgM1BXlkcykJA44Wi++KOsy90ZC6K+tkzRiCFac6eMuXK+zmc6U+6k Sz1uivJ17HABybzi9dVodsObRPJX4ymP85NMgTGNPM0KSKWrC5S1LvOFIHBfID4D C0tqj/5po/g8c50wpAT9yL/+qjkDLs1u0rnCKJAjwhWVSa2Wh2ZFKDk62TDm73JI QwxGzqqxWUGfKCg8JgcS6EQYu952hHYcsQdXoQIDAQABAoIBADjnhcwug77z8Ash 3yWKKEK0owewkpTj3i6rqv3QZDY/j76GzjYikEzUsDNjIxOh4iFqVKgZ4Vf03xDm 9yi2QiXGq8enUREZWxj6mo/2SR1T3GWGSP7PRX5iOo2zmdy7gOF+aVMxinIBYapO 6xtgz0f52bCYt7OKmLSv6t76SingEzk9FGTlCtCaKMyoHEA8qC+WKNqTEndh2DOn oMGX0zPWTmN/9P+epBGAxRVpYkmtCVQlY4u85zteuxs8SWSnkhbwwCazzapqOqKe f/lr/KJiFZ42ta6Zzh4zUvqvA2S8IrP33wm86x7ZyaBhJ8buVKEkZ4Fm5RdEPun4 Uo+Rm+0CgYEA9/dmzwdWfMmGRJoc3rdDJelbVhQeJ8eGNlOnvs4KEUK+HITEzf+u 7F3PohRF8HmgguQKuX6exLkCME9/mAomAqivitrB5aH93ubMjYzleFbwvZRLQgA7 zitdAwdoLKdw2RTo1JQtnw+I+dFDiUyolPANtGlQRJPsk56C+gBekAUCgYEA95Ax Al0byJ9Fdl91LM2MUdc3hLAzqpFnlA6DELnwkx7axshMPh5+O7+cmLMskCDIuXrI w2KNbkKSlpJituxFR37eiSgKsT13amf1pOA35iTA5pAWf6jgD6zWmhK599c/xwFe 1txa9tgCZZVSLRT7RpQ/bFWScY7puRWsaW+CZ+0CgYBkM4pM8KcnZ/wk7q3p5d5x jHoHL7v70SnP/EAV34a78N+IALLSl5alF0eXNGAKy+tr2SDoUl1wG17iDM3/r2Iq wuWk0790vuAq2dMhBCWaWm+P/EPpGNUR+/3rAmw7VzJH1qY7eOFynEF6yfBjpCGf hd3T4Ja6D5iTEoPU6J9NzQKBgQD2SF5ZSaOQTNLf1ktNzRchfAfhWyGrOIhgxKcj BrgMxI9FXpJq4g8XBaKWTvmwUM0fqMT9i5fD7zrBzNOjwx2Q6OgebtVkSg/4rX+1 DNLPhBTbytB3I9vz/DBqiuKza1IpenWNLx7Xn0GTKZ/c9ZidOHJ4JhFQI6rk1Gj3 Y1XKaQKBgB2GSNS5kkAQra12wZbmKSH61yzIxWtv58txy31iTguhSsQy6HaFvBFZ sRU0N3JOGqm8djALzJy/4nNmBoeOUWLy7a16w5qH5ED5CWGL7mtWsZ5+Sn+G9ECB XaWMJ1SovPLMiNg7dxlWuWQbXRI26bN5BBZVkAdUl0ps1KBDeogM\n-----END RSA PRIVATE KEY-----"
-  PRIVATE_KEY = OpenSSL::PKey::RSA.new(KEY_TEXT)
-  PUBLIC_KEY = PRIVATE_KEY.public_key
-  FORWARD_OAUTH_AUTH_URL = ENV['FORWARD_OAUTH_AUTH_URL']
-  AUTH_VERIFY_KEY = ENV['AUTH_VERIFY_KEY']
-  AUTH_SCOPE = ENV['AUTH_SCOPE']
-  AUTH_BOT = ENV['AUTH_BOT']
-  AUTH_CODES = {}
-
   FORWARD_AUTH[:method] = -> do
     path = request.env['HTTP_X_FORWARDED_URI']
     query = path[/\?(.*)/, 1].to_s.split('&state=', 2)
     parsed_params = Rack::Utils.parse_nested_query(query[0].to_s).merge({'state' => query[1]})
     $stdout.puts "parsed_params: #{parsed_params}"
-    if parsed_params.key?('code') && AUTH_CODES.key?(parsed_params['code'])
-      attributes = AUTH_CODES[parsed_params['code']].slice(:scope, :login)
-      generate_token attributes.merge(email: "#{attributes[:login]}@local.net")
+    if parsed_params.key?('code')
+      LOGGER.info "FORWARD_AUTH code: #{code}"
+      LOGGER.info "AUTH_CODES: #{AUTH_CODES}"
+      if AUTH_CODES.key?(parsed_params['code'])
+        attributes = AUTH_CODES[parsed_params['code']].slice(:scope, :login)
+        generate_token attributes.merge(email: "#{attributes[:login]}@local.net")
 
-      AUTH_CODES.delete parsed_params['code']
-      redirect parsed_params['state']
+        AUTH_CODES.delete parsed_params['code']
+        redirect parsed_params['state']
+      else
+        LOGGER.info "AUTH_CODES not found: #{code}"
+        halt 404, "AUTH_CODES not found: #{code}"
+      end
     else
       proto, host, path = %w[PROTO HOST URI].map { request.env["HTTP_X_FORWARDED_#{it}"] }
       full_uri = "#{proto}://#{host}#{path}"
@@ -63,31 +69,23 @@ module AuthForward
           }
           access_token = JWT.encode(data, PRIVATE_KEY, 'RS256')
           response.set_cookie('auth_token', value: access_token, path: '/', expires: Time.now + 12 * 3600, httponly: true)
-          set_x_token(data)
+          headers['X-Token'] = token.to_json
           access_token
         end
-
-        def set_x_token(token)
-          $stdout.puts "set X-Token: #{token.to_json}"
-          headers['X-Token'] = token.to_json
-        end
-
       end
 
       get '/auth' do
-
-        $stdout.puts "=== Request Environment ==="
-        request.env.each do |key, value|
-          $stdout.puts "#{key}: #{value}"
-        end
-        $stdout.puts "=========================="
+        # $stdout.puts "=== Request Environment ==="
+        # request.env.each do |key, value|
+        #   $stdout.puts "#{key}: #{value}"
+        # end
+        # $stdout.puts "=========================="
         token = request.cookies['auth_token']
         if valid_token? token
           status 200
           'OK'
         else
           instance_exec &FORWARD_AUTH[:method]
-
         end
       end
 
@@ -113,6 +111,8 @@ module AuthForward
             LOGGER.info "Slim auth LOGIN Successful: #{message}"
             authorization_code = SecureRandom.hex(16)
             AUTH_CODES[authorization_code] = { scope:, time:, login: }
+            LOGGER.info "AUTH_CODES[#{authorization_code}]: #{AUTH_CODES[authorization_code]}"
+
             redirect "#{redirect_uri}?code=#{authorization_code}&state=#{state}"
           else
             LOGGER.info "Slim auth LOGIN failed: #{message}"
