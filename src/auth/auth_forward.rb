4976325c8baa7f5f7cf89f1b011c731c27a1bb05
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2025-09-04 03:41:54 +0300

    update

--- src/auth/auth_forward.rb
+++ src/auth/auth_forward.rb
@@ -1,15 +1,25 @@
 require 'net/http'
 require 'jwt'
+require 'jwt/eddsa'
 require 'json'
 require 'digest'
-require "ed25519"
+require 'ed25519'
 require 'faraday'
 
 $stdout.sync=true
 FORWARD_AUTH = {}
-KEY_TEXT = "-----BEGIN RSA PRIVATE KEY-----\n MIIEowIBAAKCAQEA78te1DlUytbaOmSJt2v7jammv1+DYlS3Q+KlJjlKrABxvpYT WTeS/NfuY2tq3OBe/9BuEB2WS5laEJWpQV4ypEhN5agFgTPRNTmErY75adqWsf3t ISKzLTeIdjCgM1BXlkcykJA44Wi++KOsy90ZC6K+tkzRiCFac6eMuXK+zmc6U+6k Sz1uivJ17HABybzi9dVodsObRPJX4ymP85NMgTGNPM0KSKWrC5S1LvOFIHBfID4D C0tqj/5po/g8c50wpAT9yL/+qjkDLs1u0rnCKJAjwhWVSa2Wh2ZFKDk62TDm73JI QwxGzqqxWUGfKCg8JgcS6EQYu952hHYcsQdXoQIDAQABAoIBADjnhcwug77z8Ash 3yWKKEK0owewkpTj3i6rqv3QZDY/j76GzjYikEzUsDNjIxOh4iFqVKgZ4Vf03xDm 9yi2QiXGq8enUREZWxj6mo/2SR1T3GWGSP7PRX5iOo2zmdy7gOF+aVMxinIBYapO 6xtgz0f52bCYt7OKmLSv6t76SingEzk9FGTlCtCaKMyoHEA8qC+WKNqTEndh2DOn oMGX0zPWTmN/9P+epBGAxRVpYkmtCVQlY4u85zteuxs8SWSnkhbwwCazzapqOqKe f/lr/KJiFZ42ta6Zzh4zUvqvA2S8IrP33wm86x7ZyaBhJ8buVKEkZ4Fm5RdEPun4 Uo+Rm+0CgYEA9/dmzwdWfMmGRJoc3rdDJelbVhQeJ8eGNlOnvs4KEUK+HITEzf+u 7F3PohRF8HmgguQKuX6exLkCME9/mAomAqivitrB5aH93ubMjYzleFbwvZRLQgA7 zitdAwdoLKdw2RTo1JQtnw+I+dFDiUyolPANtGlQRJPsk56C+gBekAUCgYEA95Ax Al0byJ9Fdl91LM2MUdc3hLAzqpFnlA6DELnwkx7axshMPh5+O7+cmLMskCDIuXrI w2KNbkKSlpJituxFR37eiSgKsT13amf1pOA35iTA5pAWf6jgD6zWmhK599c/xwFe 1txa9tgCZZVSLRT7RpQ/bFWScY7puRWsaW+CZ+0CgYBkM4pM8KcnZ/wk7q3p5d5x jHoHL7v70SnP/EAV34a78N+IALLSl5alF0eXNGAKy+tr2SDoUl1wG17iDM3/r2Iq wuWk0790vuAq2dMhBCWaWm+P/EPpGNUR+/3rAmw7VzJH1qY7eOFynEF6yfBjpCGf hd3T4Ja6D5iTEoPU6J9NzQKBgQD2SF5ZSaOQTNLf1ktNzRchfAfhWyGrOIhgxKcj BrgMxI9FXpJq4g8XBaKWTvmwUM0fqMT9i5fD7zrBzNOjwx2Q6OgebtVkSg/4rX+1 DNLPhBTbytB3I9vz/DBqiuKza1IpenWNLx7Xn0GTKZ/c9ZidOHJ4JhFQI6rk1Gj3 Y1XKaQKBgB2GSNS5kkAQra12wZbmKSH61yzIxWtv58txy31iTguhSsQy6HaFvBFZ sRU0N3JOGqm8djALzJy/4nNmBoeOUWLy7a16w5qH5ED5CWGL7mtWsZ5+Sn+G9ECB XaWMJ1SovPLMiNg7dxlWuWQbXRI26bN5BBZVkAdUl0ps1KBDeogM\n-----END RSA PRIVATE KEY-----"
-PRIVATE_KEY = OpenSSL::PKey::RSA.new(KEY_TEXT)
-PUBLIC_KEY = PRIVATE_KEY.public_key
+
+KEY_FILENAME = ENV['RACK_ENV'] == 'production' ? '/private_keys/signing_key' : "#{__dir__}/../signing_key"
+
+if File.exist?(KEY_FILENAME)
+  signature_key_hex = IO.read(KEY_FILENAME)
+  SIGNING_KEY = Ed25519::SigningKey.new([signature_key_hex].pack('H*'))
+else
+  SIGNING_KEY = Ed25519::SigningKey.generate
+  signature_key_hex = SIGNING_KEY.to_bytes.unpack1('H*')
+  IO.write(KEY_FILENAME, signature_key_hex)
+end
+
 FORWARD_OAUTH_AUTH_URL = ENV['FORWARD_OAUTH_AUTH_URL']
 AUTH_VERIFY_KEY = ENV['AUTH_VERIFY_KEY']
 AUTH_SCOPE = ENV['AUTH_SCOPE']
@@ -70,7 +80,7 @@ module AuthForward
             iat: Time.now.to_i
           }
           # TODO: use alg: 'EdDSA' ED25519 is an EdDSA (Edwards-curve DSA) signature scheme. See also RFC8037 and RFC8032. )
-          access_token = JWT.encode(data, PRIVATE_KEY, 'RS256')
+          access_token = JWT.encode(data, SIGNING_KEY, 'EdDSA')
           response.set_cookie('auth_token', value: access_token, path: '/', expires: Time.now + 12 * 3600, httponly: true)
           access_token
         end
@@ -92,13 +102,17 @@ module AuthForward
         token = request.cookies['auth_token']
 
         redirect_uri = params[:redirect_uri]
+        r_uri = URI.parse redirect_uri
+        # "#{r_uri.scheme}://#{r_uri.user}:#{r_uri.password}@#{r_uri.host}:#{r_uri.port}"
+
         state = params[:state]
 
-        scope = AUTH_SCOPE || request.env['HTTP_HOST']
+        scope = AUTH_SCOPE || r_uri.host || request.env['HTTP_HOST']
+
         signature = params[:signature]
 
         if valid_token? token
-          decoded = JWT.decode(token, PUBLIC_KEY, true, { algorithm: 'RS256' }).first
+          decoded = JWT.decode(token, SIGNING_KEY.verify_key, true, { algorithm: 'EdDSA' }).first
           LOGGER.info 'AUTHORIZE BY TOKEN'
           authorization_code = SecureRandom.hex(16)
           AUTH_CODES[authorization_code] = { scope:, time: Time.now.to_i, login: decoded['login']}
@@ -155,7 +169,7 @@ module AuthForward
       def valid_token?(token)
         return false if !token || token.empty?
 
-        decoded = JWT.decode(token, PUBLIC_KEY, true, { algorithm: 'RS256' }).first
+        decoded = JWT.decode(token, SIGNING_KEY.verify_key, true, { algorithm: 'EdDSA' }).first
 
         # return false unless decoded['iss'] == FORWARD_OAUTH_AUTH_URL
         return false unless decoded['exp'].to_i > Time.now.to_i
